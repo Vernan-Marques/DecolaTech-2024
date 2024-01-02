db4ece357a64d84e029377a0e4988f696db65df7
Vernan <vernanmarques2019@gmail.com>
2024-01-01 22:37:33 -0300

    adicionando a pasta Exceções e Coleções em C#

---
+++ Exceções-e-Coleções-em-C#/Program.cs
@@ -0,0 +1,65 @@
+﻿using Exemplos_Explorando.Models;
+using System.Globalization;
+
+
+
+
+/*
+// Sem exceções
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");//ReadAllLines - lê linha por linha
+
+foreach (var linha in linhas)
+{
+    // será impresso cada linha
+    Console.WriteLine(linha);
+}
+*/
+
+// Tratando uma Exceção
+
+/*
+//caso 1
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+foreach (var linha in linhas)
+{
+    Console.WriteLine(linha);
+}
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+*/
+
+/*
+//caso 2
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+foreach (var linha in linhas)
+{
+    Console.WriteLine(linha);
+}
+}
+catch (FileNotFoundException ex) // Cao o erro for no arquivo
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex) // Cao o erro for na pasta
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
+}
+catch (Exception ex) // Excessão generica
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+
+finally // Finaliza
+{
+Console.WriteLine("Chegou até aqui.");
+}
+*/
